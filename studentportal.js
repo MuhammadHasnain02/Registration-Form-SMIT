e628eda923a22cc2d76de3da30ad7ebe87055a40
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-09-09 12:03:41 -0700

    form

--- studentportal.js
+++ studentportal.js
@@ -1,32 +1,28 @@
 // Logo & Button Groups Section
 let loginField = document.getElementById("loginField")
 let createAccField = document.getElementById("createAccField")
+let LoginStdBtn = document.getElementById("LoginStdBtn")
+let LoginTeachrBtn = document.getElementById("LoginTeachrBtn")
 
 // LogIn And Create Account Section
+let btnGroupSect = document.getElementById("btnGroupSect")
 let LoginSect = document.getElementById("LoginSect")
 let CrtAccSect = document.getElementById("CrtAccSect")
+let LoginTeachrSect = document.getElementById("LoginTeachrSect")
 
 // Show Section
 function showSection(Section) {
-
+    
+    btnGroupSect.classList.add("hidden")
     LoginSect.classList.add("hidden")
     CrtAccSect.classList.add("hidden")
+    LoginTeachrSect.classList.add("hidden")
 
     Section.classList.remove("hidden")
 
 }
 
-// registrationField Handling
-loginField.onclick = () => {
-    showSection(LoginSect);
-    showBgWhite(loginField)
-}
-// downloadIDCardField Handling
-createAccField.onclick = () => {
-    showSection(CrtAccSect);
-    showBgWhite(createAccField)
-}
-
+// Show Bg-white Section
 function showBgWhite(field) {
 
     loginField.classList.remove("bg-white", "shadow-md")
@@ -37,4 +33,37 @@ function showBgWhite(field) {
 
     field.classList.remove("bg-[#F5F5F5]")
     field.classList.add("bg-white", "shadow-md")
+
 }
+
+// 🔹 Handlers
+
+loginField.onclick = () => {
+
+    showSection(LoginSect);
+    btnGroupSect.classList.remove("hidden")
+    showBgWhite(loginField)
+
+};
+
+createAccField.onclick = () => {
+
+    showSection(CrtAccSect);
+    btnGroupSect.classList.remove("hidden")
+    showBgWhite(createAccField)
+
+};
+
+LoginStdBtn.onclick = () => {
+
+    showSection(LoginSect);
+    btnGroupSect.classList.remove("hidden")
+
+};
+
+LoginTeachrBtn.onclick = () => {
+
+    showSection(LoginTeachrSect);
+    btnGroupSect.classList.add("hidden")
+    
+};
